5a08fe745eeac7fa99f288a78354e6a5988cd0f6
Ajay <misdeptho@gmail.com>
2024-06-10 16:33:14 -0400

    mobile pic upload

--- Controller/Uploads.js
+++ Controller/Uploads.js
@@ -47,3 +47,48 @@ export const uploadProfilePic = async (req, res) => {
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
+
+
+
+export const uploadMobilePic = async (req, res) => {
+  try {
+    const AppId = req.headers.authorization ? req.headers.authorization.split(" ")[1] : null;
+
+    if (!AppId) {
+      return res.status(404).json({ success: false, message: "AppId not found in session" });
+    }
+
+    // Validate if AppId is a number
+    if (isNaN(AppId)) {
+      return res.status(400).json({ success: false, message: "Invalid AppId" });
+    }
+
+    // Check if file is uploaded
+    if (!req.file) {
+      return res.status(400).json({ success: false, message: "No file uploaded" });
+    }
+
+    const fileName = req.file.filename; // Use multer's generated filename
+    const query = `UPDATE ApplicationForm SET MobilePic = @MobilePic WHERE AppId = @AppId`;
+    const request = pool.request();
+    request.input("MobilePic", fileName);
+    request.input("AppId", AppId);
+    
+    const result = await request.query(query);
+
+    // Check if any rows were affected
+    if (result.rowsAffected && result.rowsAffected[0] > 0) {
+      console.log("MobilePic updated successfully");
+      res.status(200).json({
+        success: true,
+        message: "MobilePic updated successfully",
+      });
+    } else {
+      console.error("Failed to update MobilePic");
+      res.status(404).json({ success: false, message: "Failed to update MobilePic" });
+    }
+  } catch (error) {
+    console.error("Error updating MobilePic:", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
--- Router/employe/UploadeRoutes.js
+++ Router/employe/UploadeRoutes.js
@@ -1,7 +1,7 @@
 
 import express from "express"
 
-import { uploadProfilePic } from "../../Controller/Uploads.js";
+import { uploadProfilePic,uploadMobilePic } from "../../Controller/Uploads.js";
 import multipleUpload from "../../Middleware/Multer.js";
 
 
@@ -9,4 +9,5 @@ const router = express.Router();
 
 
 router.post("/profilepic", multipleUpload.single('Pic'), uploadProfilePic);
+router.post("/mobilepic", multipleUpload.single('Pic'), uploadMobilePic);
 export default router;
