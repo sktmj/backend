5d3cf2178e304114d1abc1a37689fe76b0f948ec
Ajay <misdeptho@gmail.com>
2024-08-13 17:55:41 -0400

    some change

--- Router/admin/leaveRoute.js
+++ Router/admin/leaveRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { employeIdcontroller, getLeaveController, getLeaveReportController, insertLeaveController, leavePhotoController, OrganisationController, RequestController } from "../../Controller/admin/leaveController.js";
+import multipleUpload from "../../Middleware/Multer.js";
 
 const router = express.Router();
 
