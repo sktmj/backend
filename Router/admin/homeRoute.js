e28ad46bc98fb1c0d684bf5e06b2de4c4ccb7caa
Ajay <misdeptho@gmail.com>
2024-08-20 16:45:15 -0400

    ahs

--- Router/admin/homeRoute.js
+++ Router/admin/homeRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { HomeController } from "../../Controller/admin/homeController";
+import { HomeController } from "../../Controller/admin/homeController.js";
 
 
 
